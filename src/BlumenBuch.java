13e0ddaeedfdf7ceb9ca3eef2da62bdfb080b101
Noah <noah.buchs@outlook.com>
2021-04-23 11:12:21 +0200

    Array with Flowers added

--- src/BlumenBuch.java
+++ src/BlumenBuch.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
 public class BlumenBuch {
 
     public String getInformationenUeber(final String blumenName) {
         return blumenName;
+    }
+    Blume sonnenblume = new Blume(5.4, 50, true, 20, true);
+    BlumeMitSpitznameEinheimisch tulpe = new BlumeMitSpitznameEinheimisch(true, "tulpina", 6.3, 20, true, 16, false);
+    BlumeMitSpitzname rose = new BlumeMitSpitzname("rousi", 3, 15, true, 2, false);
+    BlumeMitStachel loewenzahn = new BlumeMitStachel(12, 7.5, 10, true, 25, false);
+    ArrayList<Blume> blumen = new ArrayList<Blume>();
+
+
+
+    public void addBlume(Blume neueBlume) {
+        blumen.add(sonnenblume);
+        blumen.add(tulpe);
+        blumen.add(rose);
+        blumen.add(loewenzahn);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
-    Blume sonnenblume = new Blume(5.4,50,true,20,true);
-    BlumeMitSpitznameEinheimisch tulpe = new BlumeMitSpitznameEinheimisch(true, "tulpina", 6.3,20,true,16,false);
-    BlumeMitSpitzname rose = new BlumeMitSpitzname("rousi", 3,15,true,2,false);
-    BlumeMitStachel loewenzahn = new BlumeMitStachel(12,7.5,10,true,25,false);
+
 }
+
+
+
