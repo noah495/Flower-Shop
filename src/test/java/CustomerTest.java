105030980c842ab41d52a99d3234103268a8ffa2
Noah <noah.buchs@outlook.com>
2021-05-21 08:24:32 +0200

    CustomerTest created

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+    @Test
+    void givenCostumerBuys20TulipWhenCostumerBuys20FlowerBoughtFlowerSizeIs20() {
+        //Arrange
+        // Customer testee = new Customer("John", )
+        //Act
+        //   int actual = boughtFlowers.size();
+        //Assert
+        //     assertEquals(20, actual);
+    }
+}
